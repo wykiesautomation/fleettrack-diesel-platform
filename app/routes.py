178c0708e4d94a357ea0a308c4bb2d425e74a3d1
wykiesautomation <wykiesautomation@gmail.com>
2026-08-02 23:11:47 +0200

    Update routes.py

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,6 @@
 import secrets, re
 from datetime import datetime, timezone, timedelta
-from flask import Blueprint, render_template, request, redirect, url_for, flash, jsonify, abort, session
+from flask import Blueprint, render_template, request, redirect, url_for, flash, jsonify, abort, session, current_app
 from flask_login import login_user, logout_user, login_required, current_user
 from werkzeug.security import generate_password_hash, check_password_hash
 from sqlalchemy import desc
