fec4375fd19579ce9bc6cdb7d4c2e117de97ba16
wykiesautomation <wykiesautomation@gmail.com>
2026-08-06 09:11:16 +0200

    Delete test_identity_lock.py

--- test_identity_lock.py
+++
@@ -1,4 +0,0 @@
-from pathlib import Path
-import py_compile
-for f in ['app/models.py','app/routes.py','app/__init__.py','app/device_identity_migration.py']:py_compile.compile(f,doraise=True)
-print('DEVICE_IDENTITY_LOCK_REV01 PASS')
